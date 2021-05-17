526127e8651e7300eebf3303f0f1b72023b33133
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-05-17 17:32:49 +0800

    - Add <header> - Add logo as <img> element - Add links as <li> elements - Add #hero-text elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import logo from './images/logo.svg';
+
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <nav>
+          <img src={logo} alt="loopstudios logo" />
+          <ul>
+            <li id="about" className="link">About</li>
+            <li id="careers" className="link">Careers</li>
+            <li id="events" className="link">Events</li>
+            <li id="products" className="link">Products</li>
+            <li id="support" className="link">Support</li>
+          </ul>
+        </nav>
+
+        <div id="hero-text">
+          <h1>Immersive experiences that deliver</h1>
+        </div>
+      </header>
     </div>
   );
 }
