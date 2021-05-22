049bfa86471ce785711d9f296e2ece5a14aa0f26
Derian Melvin <derianmelvin88@gmail.com>
2021-05-22 17:22:08 +0700

    - Create components/Header.jsx - Move <header> elements from App.js to Header.jsx - Import Header.jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './images/logo.svg';
 import './css/main.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav>
-          <img src={logo} alt="loopstudios logo" />
-          <ul>
-            <li id="about" className="link">About</li>
-            <li id="careers" className="link">Careers</li>
-            <li id="events" className="link">Events</li>
-            <li id="products" className="link">Products</li>
-            <li id="support" className="link">Support</li>
-          </ul>
-        </nav>
-
-        <div id="hero-text">
-          <h1>Immersive experiences that deliver</h1>
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
