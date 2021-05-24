0445daffcca4d8a6de0a13b204d6dd94c83c0072
Derian Melvin <derianmelvin88@gmail.com>
2021-05-24 11:23:43 +0700

    - Add <section> element for #about - Add <div> element for #about-image - Add <article> with <h1>, <p> elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './css/main.css';
-import Header from './components/Header';
+import "./css/main.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section id="about">
+        <div id="about-image"></div>
+        <article>
+          <h1>The Leader in Interactive VR</h1>
+          <p>
+            Founded in 2011, Loopstudios has been producing world-class virtual
+            reality projects for some of the best companies around the globe.
+            Our award-winning creations have transformed businesses through
+            digital experiences that bind to their brand.
+          </p>
+        </article>
+      </section>
     </div>
   );
 }
