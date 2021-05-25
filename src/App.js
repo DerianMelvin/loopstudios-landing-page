a5dcc1b89cb1c4de5ece97c03db66ffbda16b37e
Derian Melvin <derianmelvin88@gmail.com>
2021-05-25 10:16:33 +0700

    - Create components/About.jsx - Move #about elements from App.js to About.jsx - Import About.jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./css/main.css";
 import Header from "./components/Header";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <section id="about">
-        <div id="about-image"></div>
-        <article id="about-article">
-          <h1>The Leader in Interactive VR</h1>
-          <p>
-            Founded in 2011, Loopstudios has been producing world-class virtual
-            reality projects for some of the best companies around the globe.
-            Our award-winning creations have transformed businesses through
-            digital experiences that bind to their brand.
-          </p>
-        </article>
-      </section>
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <section id="about">
+      <div id="about-image"></div>
+      <article id="about-article">
+        <h1>The Leader in Interactive VR</h1>
+        <p>
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default About;
