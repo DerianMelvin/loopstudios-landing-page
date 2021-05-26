1f8692e3cbe3c28963903ba695c3e941e7cae52f
Derian Melvin <derianmelvin88@gmail.com>
2021-05-26 15:48:24 +0700

    - Create components/Contents.jsx - Move #contents elements from App.js to About.jsx - Import About.jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import "./css/main.css";
 import Header from "./components/Header";
 import About from "./components/About";
+import Contents from "./components/Contents";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
-      <section id="contents">
-        <h1 id="contents-heading">Our Creations</h1>
-        <button id="contents-button">
-          <h4>See All</h4>
-        </button>
-        <ul id="contents-list">
-          <li className="item">Deep Earth</li>
-          <li className="item">Night Arcade</li>
-          <li className="item">Soccer Team VR</li>
-          <li className="item">The Grid</li>
-          <li className="item">From Up Above VR</li>
-          <li className="item">Pocket Borealis</li>
-          <li className="item">The Curiosity</li>
-          <li className="item">Make it Fisheye</li>
-        </ul>
-      </section>
+      <Contents />
     </div>
   );
 }
