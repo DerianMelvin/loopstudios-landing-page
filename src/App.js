233eeeb9fd28dbb7ac7c7c08eaf1b4888e14d1eb
Derian Melvin <derianmelvin88@gmail.com>
2021-05-27 11:23:04 +0700

    - Add <footer> element - Add related elements in <footer>:   > #footer-nav   > #footer-info - Import social media icons from images/

--- src/App.js
+++ src/App.js
@@ -3,12 +3,44 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Contents from "./components/Contents";
 
+import logo from "./images/logo.svg";
+import iFacebook from './images/icon-facebook.svg';
+import iTwitter from './images/icon-twitter.svg';
+import iPinterest from './images/icon-pinterest.svg';
+import iInstagram from './images/icon-instagram.svg';
+
 function App() {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
   return (
     <div className="App">
       <Header />
       <About />
       <Contents />
+      <footer>
+        <nav id="footer-nav">
+          <img src={logo} alt="loopstudios logo" />
+          <ul>
+            <li className="link">About</li>
+            <li className="link">Careers</li>
+            <li className="link">Events</li>
+            <li className="link">Products</li>
+            <li className="link">Support</li>
+          </ul>
+        </nav>
+
+        <div id="footer-info">
+          <ul id="social-media">
+            <a href="#"><img src={iFacebook} alt="facebook" /></a>
+            <a href="#"><img src={iTwitter} alt="twitter" /></a>
+            <a href="#"><img src={iPinterest} alt="pinterest" /></a>
+            <a href="#"><img src={iInstagram} alt="instagram" /></a>
+          </ul>
+          <p id="copyright">&#169; {currentYear} Loopstudios. All rights reserved.</p>
+        </div>
+
+      </footer>
     </div>
   );
 }
